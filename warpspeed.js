178c0286507e6e78a20e4e43776d0a409191ffe4
Andras Serfozo <subztep@gmail.com>
2020-11-14 21:57:43 +0000

    Remove unwanted parts and modernise a bit

--- warpspeed.js
+++ warpspeed.js
@@ -1,130 +1,108 @@
-window.requestAnimationFrame =
-  window.requestAnimationFrame ||
-  function (callback, element) {
-    setTimeout(callback, 1000 / 60)
+class Star {
+  constructor(x, y, z) {
+    this.x = x
+    this.y = y
+    this.z = z
+    this.size = 0.5 + Math.random()
   }
-
-function timeStamp() {
-  if (window.performance.now) return window.performance.now()
-  else return Date.now()
 }
 
-function isVisible(el) {
-  var r = el.getBoundingClientRect()
-  return (
-    r.top + r.height >= 0 &&
-    r.left + r.width >= 0 &&
-    r.bottom - r.height <= (window.innerHeight || document.documentElement.clientHeight) &&
-    r.right - r.width <= (window.innerWidth || document.documentElement.clientWidth)
-  )
-}
+// modernised, without running instances
 
-function Star(x, y, z) {
-  this.x = x
-  this.y = y
-  this.z = z
-  this.size = 0.5 + Math.random()
-}
+class WarpSpeed {
+  SPEED = 0.7
+  TARGET_SPEED = this.SPEED
+  SPEED_ADJ_FACTOR = 0.03 // min: 0, max: 1
+  DENSITY = 0.7 // min: >0
+  USE_CIRCLES = true
+  DEPTH_ALPHA = true
+  WARP_EFFECT = true
+  WARP_EFFECT_LENGTH = 5 // min: 0
+  STAR_SCALE = 3 // min: >0
+  BACKGROUND_COLOR = "hsl(263,45%,7%)"
+  STAR_COLOR = "#FFFFFF"
 
-function WarpSpeed(targetId, config) {
-  this.targetId = targetId
-  if (WarpSpeed.RUNNING_INSTANCES == undefined) WarpSpeed.RUNNING_INSTANCES = {}
-  if (WarpSpeed.RUNNING_INSTANCES[targetId]) {
-    WarpSpeed.RUNNING_INSTANCES[targetId].destroy()
-  }
-  config = config || {}
-  if (typeof config == "string")
-    try {
-      config = JSON.parse(config)
-    } catch (e) {
-      config = {}
+  _targetId
+  _STAR_R
+  _STAR_G
+  _STAR_B
+  _prevW = -1
+  _prevH = -1 //width and height will be set at first draw call
+  _stars = []
+  _lastMoveTS
+  _drawRequest = null
+  _LAST_RENDER_T = 0
+  _PAUSED = false
+
+  constructor(targetId, config) {
+    this._targetId = targetId
+    const canvas = document.getElementById(this._targetId)
+    canvas.width = 1
+    canvas.height = 1
+    const ctx = canvas.getContext("2d")
+    ctx.fillStyle = this.BACKGROUND_COLOR
+    ctx.fillRect(0, 0, 1, 1)
+    ctx.fillStyle = this.STAR_COLOR
+    ctx.fillRect(0, 0, 1, 1)
+    const color = ctx.getImageData(0, 0, 1, 1).data
+    this._STAR_R = color[0]
+    this._STAR_G = color[1]
+    this._STAR_B = color[2]
+    for (let i = 0; i < this.DENSITY * 1000; i++) {
+      this._stars.push(new Star((Math.random() - 0.5) * 1000, (Math.random() - 0.5) * 1000, 1000 * Math.random()))
     }
-  this.SPEED = config.speed == undefined || config.speed < 0 ? 0.7 : config.speed
-  this.TARGET_SPEED = config.targetSpeed == undefined || config.targetSpeed < 0 ? this.SPEED : config.targetSpeed
-  this.SPEED_ADJ_FACTOR =
-    config.speedAdjFactor == undefined
-      ? 0.03
-      : config.speedAdjFactor < 0
-      ? 0
-      : config.speedAdjFactor > 1
-      ? 1
-      : config.speedAdjFactor
-  this.DENSITY = config.density == undefined || config.density <= 0 ? 0.7 : config.density
-  this.USE_CIRCLES = config.shape == undefined ? true : config.shape == "circle"
-  this.DEPTH_ALPHA = config.depthFade == undefined ? true : config.depthFade
-  this.WARP_EFFECT = config.warpEffect == undefined ? true : config.warpEffect
-  this.WARP_EFFECT_LENGTH =
-    config.warpEffectLength == undefined ? 5 : config.warpEffectLength < 0 ? 0 : config.warpEffectLength
-  this.STAR_SCALE = config.starSize == undefined || config.starSize <= 0 ? 3 : config.starSize
-  this.BACKGROUND_COLOR = config.backgroundColor == undefined ? "hsl(263,45%,7%)" : config.backgroundColor
-  var canvas = document.getElementById(this.targetId)
-  canvas.width = 1
-  canvas.height = 1
-  var ctx = canvas.getContext("2d")
-  ctx.fillStyle = this.BACKGROUND_COLOR
-  ctx.fillRect(0, 0, 1, 1)
-  ctx.fillStyle = config.starColor == undefined ? "#FFFFFF" : config.starColor
-  ctx.fillRect(0, 0, 1, 1)
-  var color = ctx.getImageData(0, 0, 1, 1).data
-  this.STAR_R = color[0]
-  this.STAR_G = color[1]
-  this.STAR_B = color[2]
-  this.prevW = -1
-  this.prevH = -1 //width and height will be set at first draw call
-  this.stars = []
-  for (var i = 0; i < this.DENSITY * 1000; i++) {
-    this.stars.push(new Star((Math.random() - 0.5) * 1000, (Math.random() - 0.5) * 1000, 1000 * Math.random()))
+    this._lastMoveTS = window.performance.now()
+    this.draw()
   }
-  this.lastMoveTS = timeStamp()
-  this.drawRequest = null
-  this.LAST_RENDER_T = 0
-  WarpSpeed.RUNNING_INSTANCES[targetId] = this
-  this.draw()
-}
 
-WarpSpeed.prototype = {
-  constructor: WarpSpeed,
-  draw: function () {
-    var TIME = timeStamp()
-    if (!document.getElementById(this.targetId)) {
-      this.destroy()
-      return
-    }
+  draw() {
+    const TIME = window.performance.now()
     this.move()
-    var canvas = document.getElementById(this.targetId)
-    if (!this.PAUSED && isVisible(canvas)) {
-      if (this.prevW != canvas.clientWidth || this.prevH != canvas.clientHeight) {
+    const canvas = document.getElementById(this._targetId)
+
+    if (!this.PAUSED) {
+      if (this._prevW != canvas.clientWidth || this._prevH != canvas.clientHeight) {
         canvas.width = (canvas.clientWidth < 10 ? 10 : canvas.clientWidth) * (window.devicePixelRatio || 1)
         canvas.height = (canvas.clientHeight < 10 ? 10 : canvas.clientHeight) * (window.devicePixelRatio || 1)
       }
+
       this.size =
         (canvas.height < canvas.width ? canvas.height : canvas.width) /
         (10 / (this.STAR_SCALE <= 0 ? 0 : this.STAR_SCALE))
+
       if (this.WARP_EFFECT) this.maxLineWidth = this.size / 30
-      var ctx = canvas.getContext("2d")
+
+      const ctx = canvas.getContext("2d")
       ctx.fillStyle = this.BACKGROUND_COLOR
       ctx.fillRect(0, 0, canvas.width, canvas.height)
-      var rgb = "rgb(" + this.STAR_R + "," + this.STAR_G + "," + this.STAR_B + ")",
-        rgba = "rgba(" + this.STAR_R + "," + this.STAR_G + "," + this.STAR_B + ","
-      for (var i = 0; i < this.stars.length; i++) {
-        var s = this.stars[i]
-        var xOnDisplay = s.x / s.z,
-          yOnDisplay = s.y / s.z
+
+      const rgb = `rgb(${this._STAR_R},${this._STAR_G},${this._STAR_B})`
+      const rgba = `rgba(${this._STAR_R},${this._STAR_G},${this._STAR_B},`
+
+      for (const s of this._stars) {
+        const xOnDisplay = s.x / s.z
+        const yOnDisplay = s.y / s.z
+
         if (!this.WARP_EFFECT && (xOnDisplay < -0.5 || xOnDisplay > 0.5 || yOnDisplay < -0.5 || yOnDisplay > 0.5))
           continue
-        var size = (s.size * this.size) / s.z
+
+        const size = (s.size * this.size) / s.z
         if (size < 0.3) continue //don't draw very small dots
+
         if (this.DEPTH_ALPHA) {
-          var alpha = (1000 - s.z) / 1000
+          const alpha = (1000 - s.z) / 1000
           ctx.fillStyle = rgba + (alpha > 1 ? 1 : alpha) + ")"
         } else {
           ctx.fillStyle = rgb
         }
+
         if (this.WARP_EFFECT) {
           ctx.beginPath()
-          var x2OnDisplay = s.x / (s.z + this.WARP_EFFECT_LENGTH * this.SPEED),
-            y2OnDisplay = s.y / (s.z + this.WARP_EFFECT_LENGTH * this.SPEED)
+          const x2OnDisplay = s.x / (s.z + this.WARP_EFFECT_LENGTH * this.SPEED)
+          const y2OnDisplay = s.y / (s.z + this.WARP_EFFECT_LENGTH * this.SPEED)
+
           if (x2OnDisplay < -0.5 || x2OnDisplay > 0.5 || y2OnDisplay < -0.5 || y2OnDisplay > 0.5) continue
+
           ctx.moveTo(canvas.width * (xOnDisplay + 0.5) - size / 2, canvas.height * (yOnDisplay + 0.5) - size / 2)
           ctx.lineTo(canvas.width * (x2OnDisplay + 0.5) - size / 2, canvas.height * (y2OnDisplay + 0.5) - size / 2)
           ctx.lineWidth = size > this.maxLineWidth ? this.maxLineWidth : size
@@ -154,26 +132,29 @@ WarpSpeed.prototype = {
           )
         }
       }
-      this.prevW = canvas.clientWidth
-      this.prevH = canvas.clientHeight
+      this._prevW = canvas.clientWidth
+      this._prevH = canvas.clientHeight
     }
-    if (this.drawRequest != -1) this.drawRequest = requestAnimationFrame(this.draw.bind(this))
-    this.LAST_RENDER_T = timeStamp() - TIME
-  },
-  move: function () {
-    var t = timeStamp(),
-      speedMulF = (t - this.lastMoveTS) / (1000 / 60)
-    this.lastMoveTS = t
-    if (this.PAUSED) return
-    var speedAdjF = Math.pow(
+    if (this._drawRequest != -1) this._drawRequest = requestAnimationFrame(this.draw.bind(this))
+    this._LAST_RENDER_T = window.performance.now() - TIME
+  }
+
+  move() {
+    const t = window.performance.now()
+    const speedMulF = (t - this._lastMoveTS) / (1000 / 60)
+    this._lastMoveTS = t
+
+    if (this._PAUSED) return
+
+    const speedAdjF = Math.pow(
       this.SPEED_ADJ_FACTOR < 0 ? 0 : this.SPEED_ADJ_FACTOR > 1 ? 1 : this.SPEED_ADJ_FACTOR,
       1 / speedMulF
     )
     this.SPEED = this.TARGET_SPEED * speedAdjF + this.SPEED * (1 - speedAdjF)
     if (this.SPEED < 0) this.SPEED = 0
-    var speed = this.SPEED * speedMulF
-    for (var i = 0; i < this.stars.length; i++) {
-      var s = this.stars[i]
+    const speed = this.SPEED * speedMulF
+
+    for (const s of this._stars) {
       s.z -= speed
       while (s.z < 1) {
         s.z += 1000
@@ -181,25 +162,5 @@ WarpSpeed.prototype = {
         s.y = (Math.random() - 0.5) * s.z
       }
     }
-  },
-  destroy: function (targetId) {
-    if (targetId) {
-      if (WarpSpeed.RUNNING_INSTANCES[targetId]) WarpSpeed.RUNNING_INSTANCES[targetId].destroy()
-    } else {
-      try {
-        cancelAnimationFrame(this.drawRequest)
-      } catch (e) {
-        this.drawRequest = -1
-      }
-      WarpSpeed.RUNNING_INSTANCES[this.targetId] = undefined
-    }
-  },
-  pause: function () {
-    this.PAUSED = true
-  },
-  resume: function () {
-    this.PAUSED = false
-  },
+  }
 }
-
-WarpSpeed.destroy = WarpSpeed.prototype.destroy
