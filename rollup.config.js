cd9805a88fccb2f023c708a1eb1c1a46d95fe468
Andras Serfozo <subztep@gmail.com>
2020-11-25 02:34:49 +0000

    Setup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,41 @@
-import { terser } from "rollup-plugin-terser"
-import webWorkerLoader from "rollup-plugin-web-worker-loader"
+import pug from "pug"
+import babel from "@rollup/plugin-babel"
+import postcss from "rollup-plugin-postcss"
+import html from "@open-wc/rollup-plugin-html"
+import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
+import { main } from "./package.json"
 
 export default {
-  input: "component.js",
-  plugins: [
-    // terser(),
-    webWorkerLoader({ targetPlatform: "browser" }),
-  ],
-  output: {
-    file: "awt.min.js",
-  },
+  input: main,
+  output: { dir: "." },
+  plugins: (() => {
+    const extensions = [".js", ".ts"]
+
+    const plugins = [
+      postcss({ extract: true, minimize: false }),
+      babel({ extensions, babelHelpers: "bundled" }),
+      resolve({ extensions, preferBuiltins: true }),
+      commonjs(),
+    ]
+
+    // console.log("JUGUU", pug.compileFile("index.pug", { pretty: true })())
+
+    plugins.push(
+      html({
+        name: "index.html",
+        inject: false,
+        template: ({ bundle }) =>
+          pug
+            .compileFile("index.pug", { pretty: true })()
+            .replace(
+              "<!--bundle-->",
+              bundle.entrypoints.map(({ importPath }) => `<script src="${importPath}"></script>`)
+            ),
+        minify: false,
+      })
+    )
+
+    return plugins
+  })(),
 }
